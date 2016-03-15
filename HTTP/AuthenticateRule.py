19e302bce80d127b9232b74ff54f9fc3c383c445
Eduardo Acha <eduardoacha@gmail.com>
2016-03-15 15:37:24 -0400

    Adding HTTP Rules
     -  Server
     -  WWW-Authenticate

---
+++ HTTP/AuthenticateRule.py
@@ -0,0 +1,15 @@
+import Rules
+
+
+class Authenticate(Rules.Rules):
+    port = '80'
+    protocol = 'http'
+
+    def apply_rule(self, obj, score):
+        try:
+            obj[self.port][self.http]['get']['headers']['www_authenticate']
+        except KeyError:
+            score.partial_scores.add(100)
+
+        score.partial_scores.add(0)
+        score.set_trim(0)
---
+++ HTTP/ServerRule.py
@@ -0,0 +1,14 @@
+import Rules
+
+
+class ServerRule(Rules.Rules):
+    port = '80'
+    protocol = 'http'
+
+    def apply_rule(self, obj, score):
+        try:
+            obj[self.port][self.http]['get']['headers']['server']
+        except KeyError:
+            score.partial_scores.add(100)
+
+        score.partial_scores.add(60)
