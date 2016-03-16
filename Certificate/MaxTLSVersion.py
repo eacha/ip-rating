cc12fecf02740bf0223b3ec8baf9a141f7067253
Eduardo Acha <eduardoacha@gmail.com>
2016-03-16 15:20:30 -0400

    Adding a rule to score the max TLS version supported

---
+++ Certificate/MaxTLSVersion.py
@@ -0,0 +1,23 @@
+import Rules
+
+
+class MaxTLSVersion(Rules.Rules):
+    port = '443'
+    protocol = 'https'
+
+    # version name -> (score, trim)
+    TLS_VERSION = {
+        'TLSv1.2': (100, 100),
+        'TLSv1.1': (90, 90),
+        'TLSv1.0': (50, 50),
+        'SSLv3.0': (0, 0),
+        'SSLv2.0': (0, 0)
+    }
+
+    def apply_rule(self, obj, score):
+        try:
+            version = obj[self.port][self.protocol]['version']
+            score.add_score(self.TLS_VERSION[version].first())
+            score.set_trim(self.TLS_VERSION[version].second())
+        except KeyError:
+            return
