867e586bdeabcce0950cd7a328098a52dda8008e
Eduardo Acha <eduardoacha@gmail.com>
2016-03-18 11:43:25 -0400

    Fixed  error in the use of list, changed the return statement of calc_avg and trim_score

--- Rules.py
+++ Rules.py
@@ -1,32 +1,36 @@
 class Score(object):
     MAX_SCORE = 100
     MIN_SCORE = 0
-    FINAL_SCORE = MIN_SCORE
 
     def __init__(self):
         self.trim = self.MAX_SCORE
         self.partial_scores = list()
 
     def add_score(self, score):
-        self.partial_scores.add(score)
+        self.partial_scores.append(score)
 
     def calc_avg(self):
         if len(self.partial_scores) == 0:
-            return
+            return None
 
+        final_score = 0
         for score in self.partial_scores:
-            self.FINAL_SCORE += score
+            final_score += score
 
-        self.FINAL_SCORE /= len(self.partial_scores)
-        self.trim_score()
+        final_score /= float(len(self.partial_scores))
+        return self.trim_score(final_score)
 
     def set_trim(self, trim):
         if trim < self.trim:
             self.trim = trim
 
-    def trim_score(self):
-        if self.FINAL_SCORE > self.trim:
-            self.FINAL_SCORE = self.trim
+    def trim_score(self, score):
+        if score > self.trim:
+            return self.trim
+        return score
+
+    def pprint(self):
+        print len(self.partial_scores)
 
 
 class Rules(object):
@@ -34,12 +38,3 @@ class Rules(object):
     protocol = None
 
     name = None
-
-    def check_open_port(self, obj):
-        try:
-            obj[self.port]
-        except KeyError:
-            return False
-
-        return True
-
