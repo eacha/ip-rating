8104211ecffaae06294d7312629e1f2efd607721
Eduardo Acha <eduardoacha@gmail.com>
2016-03-22 11:29:47 -0400

    Added to main new option to rate certificate values and ignore the None rated ips

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import argparse
 import json
+
 from HTTP import *
+from Certificate import *
 from HTTP.HTTPRule import HTTPRule
+from Certificate.CertificateRule import CertificateRule
 from Rules import Score
 
 
@@ -9,7 +12,14 @@ def argument_parser():
     parser = argparse.ArgumentParser(description='Recollect IP data')
     parser.add_argument('-i', '--input', help='Input file name', required=True)
     parser.add_argument('--http', help='Rate the ips with the port 80 open', action='store_true', required=False)
+    parser.add_argument('--certificate', help='Rate the ips with certificates', action='store_true', required=False)
     return parser.parse_args()
+
+
+def append_if_not_none(collection, value):
+    if value is not None:
+        collection.append(value)
+    return
 
 
 def avg(scores):
@@ -30,6 +40,15 @@ def http(data):
     return score.calc_avg()
 
 
+def certificate(data):
+    subclasses = CertificateRule.all_subclasses()
+    score = Score()
+
+    for sub in subclasses:
+        sub().apply_rule(json_line, score)
+    return score.calc_avg()
+
+
 if __name__ == '__main__':
     args = argument_parser()
     input_file = open(args.input, 'r')
@@ -40,6 +59,9 @@ if __name__ == '__main__':
         json_line = json.loads(line)
 
         if args.http:
-            country.append(http(json_line))
+            append_if_not_none(country, http(json_line))
+
+        if args.certificate:
+            append_if_not_none(country, certificate(json_line))
 
     print avg(country)
